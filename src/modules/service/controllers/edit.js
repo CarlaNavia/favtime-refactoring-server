3347889011eca6063049e9009774258552dd402f
Carla Navia <cnavia27@gmail.com>
2021-02-26 13:28:38 +0100

    edit service endpoint

--- src/modules/service/controllers/edit.js
+++ src/modules/service/controllers/edit.js
@@ -2,11 +2,17 @@ import ServiceRepository from "../repositories/service";
 
 export default async (req, res, next) => {
   try {
-    const serviceToUpdate = await ServiceRepository.editAService(
-      req.params.id,
-      req.body
-    );
-    res.status(200).json(serviceToUpdate);
+    const oneService = await ServiceRepository.findById(req.params.id);
+
+    if (oneService.owner == req.user.userId) {
+      const serviceToUpdate = await ServiceRepository.editAService(
+        req.params.id,
+        req.body
+      );
+      res.status(200).json(serviceToUpdate);
+    } else {
+      res.status(500).json({ message: "Something went wrong" });
+    }
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
   }
--- src/routes/service/index.js
+++ src/routes/service/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import remove from "../../modules/service/controllers/remove";
 import create from "../../modules/service/controllers/create";
 // import search from "../../modules/service/controllers/search";
-// import edit from "../../modules/service/controllers/edit";
+import edit from "../../modules/service/controllers/edit";
 import detail from "../../modules/service/controllers/detail";
 import services from "../../modules/service/controllers/services";
 
@@ -18,7 +18,7 @@ router.get("/services/:userId", isLoggedIn(), services);
 
 // router.get("/service/search", search);
 
-// router.put("/service/:id", isLoggedIn(), edit);
+router.put("/service/:id", isLoggedIn(), edit);
 
 router.delete("/service/:id", isLoggedIn(), remove);
 
